b57789b3a224db28ae12e11071934eadebe80a86
hungphamphu99 <hungphamphu99@gmail.com>
2024-11-09 01:19:07 +0700

    Initial commit

---
+++ Web-front-end/project-1/assets/js/slider-product.js
@@ -0,0 +1,26 @@
+
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:24,
+        nav:true,
+  
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:4
+            },
+            1320:{
+                items:4
+            }
+        }
+    })
+
+
+
+  });
