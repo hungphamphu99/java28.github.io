c14dcdba714a40c8ba5a5fec42997f54397088b0
hungphamphu99 <hungphamphu99@gmail.com>
2025-01-18 00:14:32 +0700

    js

---
+++ Spring-Boot/01/hw/main.js
@@ -0,0 +1,47 @@
+function calculateFactorial(n) {
+    if (n < 0) return "Invalid";
+    let factorial = 1;
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+console.log(calculateFactorial(5));
+
+
+function reverseString(str){
+    return str.split('').reverse().join('');
+}
+console.log(reverseString('hello'));
+
+
+function translate(countryCode) {
+    switch (countryCode) {
+        case 'VN':
+            return "Xin chào";
+        case 'EN':
+            return "Hello";
+        case 'FR':
+            return "Bonjour";
+        default:
+            return "Chào bạn!";
+    }
+}
+
+console.log(translate('VN'));
+console.log(translate('EN'));
+console.log(translate('FR'));
+console.log(translate('DE'));
+
+
+function subString(str) {
+    if (str.length > 15) {
+        return str.slice(0, 10) + '...';
+    }
+    return str;
+}
+
+console.log(subString("xinchaocacbandenvoiTechmaster"));
+console.log(subString("hello world"));
+
---
+++ Spring-Boot/01/main.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+
+// alert("hello")
+
+// document.write("hello");
+
+// let | const
+let name;
+name = "hung";
+console.log(typeof name)
+
+let age =21;
+let year=2003;
+console.log(`Xin chào các bạn, mình tên là ${name}. Năm nay ${2025 - year} tuổi`);
+
+function sum(a, b){
+    return a+b;
+    
+}
