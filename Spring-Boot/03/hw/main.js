b45ccecc367e118366f75e2584a0bc1aa8a9d574
hungphamphu99 <hungphamphu99@gmail.com>
2025-02-13 02:02:49 +0700

    hw

--- Spring-Boot/03/hw/main.js
+++ Spring-Boot/03/hw/main.js
@@ -12,7 +12,7 @@ console.log(products.map(toString).join('\n'));
 
 // 2. Tính tổng tiền tất cả sản phẩm trong giỏ hàng
 let totalPrice = products.reduce((sum, p) => sum + p.price * p.count, 0);
-console.log("Tổng tiền giỏ hàng:", totalPrice);
+console.log("Tổng tiền :", totalPrice);
 
 // 3. Tìm các sản phẩm của thương hiệu "Apple"
 let appleProducts = products.filter(p => p.brand.toLowerCase() === "apple");
@@ -32,7 +32,7 @@ console.log("Giỏ hàng sau khi thêm sản phẩm:", products);
 
 // 7. Xóa tất cả sản phẩm của thương hiệu "Samsung"
 products = products.filter(p => p.brand.toLowerCase() !== "samsung");
-console.log("Giỏ hàng sau khi xóa Samsung:", products);
+console.log("Giỏ hàng sau khi xóa:", products);
 
 // 8. Sắp xếp giỏ hàng theo price tăng dần
 products.sort((a, b) => a.price - b.price);
@@ -44,4 +44,4 @@ console.log("Giỏ hàng sắp xếp theo số lượng giảm dần:", products
 
 // 10. Lấy ra 2 sản phẩm bất kỳ
 let randomProducts = products.sort(() => 0.5 - Math.random()).slice(0, 2);
-console.log("2 sản phẩm ngẫu nhiên:", randomProducts);
+console.log("Lấy 2 sản phẩm ngẫu nhiên:", randomProducts);
